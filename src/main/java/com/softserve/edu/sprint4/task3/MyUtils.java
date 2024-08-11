915792e4bfa04432589e0168e2d6b5c44e72a54d
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-11 12:44:02 +0300

    add task3 compare Map value with List and if such elements from List contains in Map-> true

--- src/main/java/com/softserve/edu/sprint4/task3/MyUtils.java
+++ src/main/java/com/softserve/edu/sprint4/task3/MyUtils.java
@@ -3,13 +3,19 @@ package com.softserve.edu.sprint4.task3;
 import java.util.*;
 
 public class MyUtils {
-    // Code
     public boolean listMapCompare(List<String> list, Map<String, String> map) {
-        // Code
-        //
 
-        return false; // TODO Update
-        //
+        for (String s : map.values()) {
+            if (!list.contains(s)) {
+                return false;
+            }
+        }
+        return true;
     }
-    
+
+//    public boolean listMapCompare(List<String> list, Map<String, String> map) {
+//        return map.values().stream().allMatch(list::contains);
+//    }
+
+
 }
