43119286cbdfca4e559fa757b2ff96b8e9ad0430
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-11 12:05:26 +0300

    add task2 from two lists find same ID and output it in one copy

--- src/main/java/com/softserve/edu/sprint4/task2/MyUtils.java
+++ src/main/java/com/softserve/edu/sprint4/task2/MyUtils.java
@@ -2,6 +2,7 @@ package com.softserve.edu.sprint4.task2;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 public class MyUtils {
@@ -9,21 +10,61 @@ public class MyUtils {
     public static class Student {
         private int id;
         private String name;
-        // Constructor, metthods, Code
-        //
 
         public Student(int id, String name) {
-            // TODO
+            this.id = id;
+            this.name = name;
         }
 
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Student student = (Student) o;
+            return id == student.id && Objects.equals(name, student.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
 
     public Set<Student> commonStudents(List<Student> list1, List<Student> list2) {
-        // Code
-        //
+        Set<Student> set1 = new HashSet<>(list1);
+        Set<Student> commonSet = new HashSet<>();
 
-        return null; // TODO Update
-        //
+        for (Student student : list2) {
+            if (set1.contains(student)) {
+                commonSet.add(student);
+            }
+        }
+
+        return commonSet;
     }
 
 }
