2833ca2e6897106c0f5bd3c2f1fb91dfba2bc174
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-11 13:20:57 +0300

    add task4 generic method setAndReturn(T[] arrays, T element, int position)

--- src/main/java/com/softserve/edu/sprint4/task4/ArrayUtil.java
+++ src/main/java/com/softserve/edu/sprint4/task4/ArrayUtil.java
@@ -1,7 +1,13 @@
 package com.softserve.edu.sprint4.task4;
 
 class ArrayUtil {
-    // Write static method setAndReturn(...) here
-    //
+    public static <T> T setAndReturn(T[] arrays, T element, int position) {
+        if (position < 0 || position >= arrays.length) {
+            throw new IndexOutOfBoundsException("Position out of bounds");
+        }
+        arrays[position] = element;
+        return arrays[position];
+
+    }
 
 }
